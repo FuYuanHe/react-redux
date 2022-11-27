5d49539856bff93d5f7a95c92ebbc0e3baa7130a
afuscript <hfy8358@126.com>
2022-11-27 15:33:48 +0800

    chore:初始化

---
+++ src/index.js
@@ -0,0 +1,18 @@
+// 先单独使用redux
+
+
+
+
+
+
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
